652b8eb1674a016044d30f2534e6b0e941949730
Lassi Paavolainen <lassi.paavolainen@gmail.com>
2020-11-27 15:41:24 +0200

    Updated wrapper

--- wrapper.py
+++ wrapper.py
@@ -6,22 +6,22 @@ from maskrcnn_utils import InferenceConfig
 from maskrcnn_utils import Dataset
 from mrcnn import model as modellib
 from cytomine.models import Job
-from neubiaswg5 import CLASS_OBJSEG
-from neubiaswg5.helpers import NeubiasJob, prepare_data, upload_data, upload_metrics
+from biaflows import CLASS_OBJSEG
+from biaflows.helpers import BiaflowsJob, prepare_data, upload_data, upload_metrics
 
 
 def main(argv):
     base_path = "{}".format(os.getenv("HOME")) # Mandatory for Singularity
     problem_cls = CLASS_OBJSEG
 
-    with NeubiasJob.from_cli(argv) as nj:
-        nj.job.update(status=Job.RUNNING, progress=0, statusComment="Initialisation...")
+    with BiaflowsJob.from_cli(argv) as bj:
+        bj.job.update(status=Job.RUNNING, progress=0, statusComment="Initialisation...")
         # 1. Prepare data for workflow
-        in_imgs, gt_imgs, in_path, gt_path, out_path, tmp_path = prepare_data(problem_cls, nj, is_2d=True, **nj.flags)
+        in_imgs, gt_imgs, in_path, gt_path, out_path, tmp_path = prepare_data(problem_cls, bj, is_2d=True, **bj.flags)
         files = [image.filepath for image in in_imgs]
 
         # 2. Run Mask R-CNN prediction
-        nj.job.update(progress=25, statusComment="Launching workflow...")
+        bj.job.update(progress=25, statusComment="Launching workflow...")
 
         model_dir = "/app"
         dataset = Dataset()
@@ -34,7 +34,7 @@ def main(argv):
         model.load_weights(os.path.join(model_dir,'weights.h5'), by_name=True)
 
         for i,image_id in enumerate(dataset.image_ids):
-            tiles = dataset.load_image(image_id, nj.parameters.nuclei_major_axis)
+            tiles = dataset.load_image(image_id, bj.parameters.nuclei_major_axis)
             tile_masks = []
             for image in tiles:
                 mask = model.detect([image], verbose=0)[0]
@@ -44,16 +44,16 @@ def main(argv):
             skimage.io.imsave(os.path.join(out_path,os.path.basename(files[i])), mask_img)
 
         # 3. Upload data to BIAFLOWS
-        upload_data(problem_cls, nj, in_imgs, out_path, **nj.flags, monitor_params={
+        upload_data(problem_cls, bj, in_imgs, out_path, **bj.flags, monitor_params={
             "start": 60, "end": 90, "period": 0.1,
             "prefix": "Extracting and uploading polygons from masks"})
         
         # 4. Compute and upload metrics
-        nj.job.update(progress=90, statusComment="Computing and uploading metrics...")
-        upload_metrics(problem_cls, nj, in_imgs, gt_path, out_path, tmp_path, **nj.flags)
+        bj.job.update(progress=90, statusComment="Computing and uploading metrics...")
+        upload_metrics(problem_cls, bj, in_imgs, gt_path, out_path, tmp_path, **bj.flags)
 
         # 5. Pipeline finished
-        nj.job.update(progress=100, status=Job.TERMINATED, status_comment="Finished.")
+        bj.job.update(progress=100, status=Job.TERMINATED, status_comment="Finished.")
 
 
 if __name__ == "__main__":
